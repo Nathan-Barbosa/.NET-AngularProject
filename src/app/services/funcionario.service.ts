021e660aa133c554124c69f8be82e8d103277202
Nathan-Barbosa <nathan.s.barbosa0@gmail.com.br>
2024-08-07 14:10:06 -0300

    Angular frontend with dotnet

---
+++ src/app/services/funcionario.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Funcionario } from '../models/Funcionarios';
+import { Response } from '../models/Response';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  private apiUrl = `${environment.ApiUrl}Funcionario`
+  constructor(private http: HttpClient) { }
+
+  GetFuncionarios() : Observable<Response<Funcionario[]>> {
+
+    return this.http.get<Response<Funcionario[]>>(this.apiUrl);
+  }
+
+  CreateFuncionario(funcionario: Funcionario) : Observable<Response<Funcionario[]>>{
+    return this.http.post<Response<Funcionario[]>>(`${this.apiUrl}`, funcionario);
+  }
+}
